777bd8287cb797080f77357a5878a8b2175b20d4
German Alonso Suarez Guerrero <gersua@GERMANs-MacBook-Pro.local>
2016-01-24 11:09:58 -0500

    Recursive unzip

--- Capstone-ZIP-Util/src/main/java/co/gersua/cloudmooc/capstone/zip/Main.java
+++ Capstone-ZIP-Util/src/main/java/co/gersua/cloudmooc/capstone/zip/Main.java
@@ -5,8 +5,8 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) {
-        String zipFile = "/Users/gersua/Desktop/unetbootin-mac-613.zip";
-        ZipUtil.unzip(zipFile);
+        String zipFile = "/Users/gersua/Desktop/ZipTest";
+        ZipUtil.deepUnzip(zipFile);
     }
 }
 
--- Capstone-ZIP-Util/src/main/java/co/gersua/cloudmooc/capstone/zip/ZipUtil.java
+++ Capstone-ZIP-Util/src/main/java/co/gersua/cloudmooc/capstone/zip/ZipUtil.java
@@ -1,17 +1,53 @@
 package co.gersua.cloudmooc.capstone.zip;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 public class ZipUtil {
 
-    public static void unzip(String zipFileName) {
+    private static final FileFilter DIRECTORIES_FILTER = file -> file.isDirectory();
+    private static final FileFilter ZIP_FILES_FILTER = file -> file.getName().toLowerCase().endsWith(".zip");
 
-        File zipFile = new File(zipFileName);
+    /**
+     * Recursive unzips all zip from the folder given
+     * @param folder Initial place in the file system where it start looking recursive .zip files
+     */
+    public static void deepUnzip(String folder) {
+
+        File folderFile = new File(folder);
+        if (!folderFile.exists() || !folderFile.isDirectory()) {
+            // TODO Log here
+            return;
+        }
+
+        List<File> zipFiles = getZipFiles(folderFile);
+        zipFiles.parallelStream().forEach(zip -> unzip(zip));
+    }
+
+    private static List<File> getZipFiles(File parentDirectory) {
+        List<File> zipList = new ArrayList<>();
+
+        if (parentDirectory.exists() && parentDirectory.isDirectory()) {
+            zipList.addAll(Arrays.asList(parentDirectory.listFiles(ZIP_FILES_FILTER)));
+
+            File[] directories = parentDirectory.listFiles(DIRECTORIES_FILTER);
+            for (File directory : directories) {
+                zipList.addAll(getZipFiles(directory));
+            }
+        }
+
+        return zipList;
+    }
+
+    /**
+     * Unzips a given zip file
+     * @param zipFile File to unzip
+     */
+    public static void unzip(File zipFile) {
         if (!zipFile.exists()) {
             // TODO Log here
             return;
@@ -49,5 +85,14 @@ public class ZipUtil {
         } catch (IOException ex) {
             // TODO Log here
         }
+    }
+
+    /**
+     * Unzips a given zip file given its name
+     * @param zipFileName Complete path to the file to unzip
+     */
+    public static void unzip(String zipFileName) {
+        File zipFile = new File(zipFileName);
+        unzip(zipFile);
     }
 }
