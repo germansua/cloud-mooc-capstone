f43b2565b6321cccc7cfc82ba0a6935357206236
German Suarez <German.Suarez@prodigious.com>
2016-02-02 00:05:25 -0500

    REducer

--- Capstone-Hadoop-G2Q3/src/main/java/co/gersua/cloudmooc/mapred/g2q3/Top10CarriersOnTimeReducer.java
+++ Capstone-Hadoop-G2Q3/src/main/java/co/gersua/cloudmooc/mapred/g2q3/Top10CarriersOnTimeReducer.java
@@ -2,6 +2,7 @@ package co.gersua.cloudmooc.mapred.g2q3;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,13 +14,15 @@ import org.apache.hadoop.mapreduce.Reducer;
 public class Top10CarriersOnTimeReducer extends Reducer<NullWritable, TextArrayWritable, Text, DoubleWritable> {
 
     private static final int MAX_RESULTS = 10;
+    private static final List<String> allowedRoutes =
+            Arrays.asList("\"CMI\":\"ORD\"", "\"IND\":\"CMH\"", "\"DFW\":\"IAH\"", "\"LAX\":\"SFO\"", "\"JFK\":\"LAX\"", "\"ATL\":\"PHX\"");
 
-    private List<Pair<Double, String>> resultsCMI = new ArrayList<Pair<Double, String>>();
-    private List<Pair<Double, String>> resultsBWI = new ArrayList<Pair<Double, String>>();
-    private List<Pair<Double, String>> resultsMIA = new ArrayList<Pair<Double, String>>();
-    private List<Pair<Double, String>> resultsLAX = new ArrayList<Pair<Double, String>>();
-    private List<Pair<Double, String>> resultsIAH = new ArrayList<Pair<Double, String>>();
-    private List<Pair<Double, String>> resultsSFO = new ArrayList<Pair<Double, String>>();
+    private List<Pair<Double, String>> CMI_ORD = new ArrayList<Pair<Double, String>>();
+    private List<Pair<Double, String>> IND_CMH = new ArrayList<Pair<Double, String>>();
+    private List<Pair<Double, String>> DFW_IAH = new ArrayList<Pair<Double, String>>();
+    private List<Pair<Double, String>> LAX_SFO = new ArrayList<Pair<Double, String>>();
+    private List<Pair<Double, String>> JFK_LAX = new ArrayList<Pair<Double, String>>();
+    private List<Pair<Double, String>> ATL_PHX = new ArrayList<Pair<Double, String>>();
 
     @Override
     public void reduce(NullWritable key, Iterable<TextArrayWritable> values, Context context)
@@ -27,32 +30,38 @@ public class Top10CarriersOnTimeReducer extends Reducer<NullWritable, TextArrayW
 
         for (TextArrayWritable value : values) {
             Text[] pairs = (Text[]) value.toArray();
-            String airportCarrier = pairs[0].toString();
-            Double departureDelay = Double.valueOf(pairs[1].toString());
+            String sourceDestCarrier = pairs[0].toString();
+            Double arrivalPerformance = Double.valueOf(pairs[1].toString());
 
-            if (airportCarrier.contains("\"CMI\"")) {
-                resultsCMI.add(new Pair<Double, String>(departureDelay, airportCarrier));
-            } else if (airportCarrier.contains("\"BWI\"")) {
-                resultsBWI.add(new Pair<Double, String>(departureDelay, airportCarrier));
-            } else if (airportCarrier.contains("\"MIA\"")) {
-                resultsMIA.add(new Pair<Double, String>(departureDelay, airportCarrier));
-            } else if (airportCarrier.contains("\"LAX\"")) {
-                resultsLAX.add(new Pair<Double, String>(departureDelay, airportCarrier));
-            } else if (airportCarrier.contains("\"IAH\"")) {
-                resultsIAH.add(new Pair<Double, String>(departureDelay, airportCarrier));
-            } else if (airportCarrier.contains("\"SFO\"")) {
-                resultsSFO.add(new Pair<Double, String>(departureDelay, airportCarrier));
+            if (sourceDestCarrier.contains(allowedRoutes.get(0))) {
+                CMI_ORD.add(new Pair<Double, String>(arrivalPerformance, sourceDestCarrier));
+            } else if (sourceDestCarrier.contains(allowedRoutes.get(1))) {
+                IND_CMH.add(new Pair<Double, String>(arrivalPerformance, sourceDestCarrier));
+            } else if (sourceDestCarrier.contains(allowedRoutes.get(2))) {
+                DFW_IAH.add(new Pair<Double, String>(arrivalPerformance, sourceDestCarrier));
+            } else if (sourceDestCarrier.contains(allowedRoutes.get(3))) {
+                LAX_SFO.add(new Pair<Double, String>(arrivalPerformance, sourceDestCarrier));
+            } else if (sourceDestCarrier.contains(allowedRoutes.get(4))) {
+                JFK_LAX.add(new Pair<Double, String>(arrivalPerformance, sourceDestCarrier));
+            } else if (sourceDestCarrier.contains(allowedRoutes.get(5))) {
+                ATL_PHX.add(new Pair<Double, String>(arrivalPerformance, sourceDestCarrier));
             }
         }
 
-        organizeList(resultsCMI, resultsBWI, resultsMIA, resultsLAX, resultsIAH, resultsSFO);
+        organizeList(CMI_ORD, IND_CMH, DFW_IAH, LAX_SFO, JFK_LAX, ATL_PHX);
 
-        List<Pair<Double, String>> resultsCMISublist = resultsCMI.subList(0, resultsCMI.size() > MAX_RESULTS ? MAX_RESULTS : resultsCMI.size());
-        List<Pair<Double, String>> resultsBWISublist = resultsBWI.subList(0, resultsBWI.size() > MAX_RESULTS ? MAX_RESULTS : resultsBWI.size());
-        List<Pair<Double, String>> resultsMIASublist = resultsMIA.subList(0, resultsMIA.size() > MAX_RESULTS ? MAX_RESULTS : resultsMIA.size());
-        List<Pair<Double, String>> resultsLAXSublist = resultsLAX.subList(0, resultsLAX.size() > MAX_RESULTS ? MAX_RESULTS : resultsLAX.size());
-        List<Pair<Double, String>> resultsIAHSublist = resultsIAH.subList(0, resultsIAH.size() > MAX_RESULTS ? MAX_RESULTS : resultsIAH.size());
-        List<Pair<Double, String>> resultsSFOSublist = resultsSFO.subList(0, resultsSFO.size() > MAX_RESULTS ? MAX_RESULTS : resultsSFO.size());
+        List<Pair<Double, String>> resultsCMISublist = CMI_ORD
+                .subList(0, CMI_ORD.size() > MAX_RESULTS ? MAX_RESULTS : CMI_ORD.size());
+        List<Pair<Double, String>> resultsBWISublist = IND_CMH
+                .subList(0, IND_CMH.size() > MAX_RESULTS ? MAX_RESULTS : IND_CMH.size());
+        List<Pair<Double, String>> resultsMIASublist = DFW_IAH
+                .subList(0, DFW_IAH.size() > MAX_RESULTS ? MAX_RESULTS : DFW_IAH.size());
+        List<Pair<Double, String>> resultsLAXSublist = LAX_SFO
+                .subList(0, LAX_SFO.size() > MAX_RESULTS ? MAX_RESULTS : LAX_SFO.size());
+        List<Pair<Double, String>> resultsIAHSublist = JFK_LAX
+                .subList(0, JFK_LAX.size() > MAX_RESULTS ? MAX_RESULTS : JFK_LAX.size());
+        List<Pair<Double, String>> resultsSFOSublist = ATL_PHX
+                .subList(0, ATL_PHX.size() > MAX_RESULTS ? MAX_RESULTS : ATL_PHX.size());
 
         writeList(context,
                 resultsCMISublist, resultsBWISublist, resultsMIASublist, resultsLAXSublist, resultsIAHSublist, resultsSFOSublist);
@@ -70,9 +79,9 @@ public class Top10CarriersOnTimeReducer extends Reducer<NullWritable, TextArrayW
 
         for (List<Pair<Double, String>> list : lists) {
             for (Pair<Double, String> item : list) {
-                Text airport = new Text(item.y);
-                DoubleWritable totalDelay = new DoubleWritable(item.x);
-                context.write(airport, totalDelay);
+                Text sourceDestCarrier = new Text(item.y);
+                DoubleWritable arrivalPerformance = new DoubleWritable(item.x);
+                context.write(sourceDestCarrier, arrivalPerformance);
             }
         }
     }
